88392e153406fbe5a49012995a01f1b1b12b8276
Grady Simon <grady@gradysimon.com>
2015-08-21 16:47:34 -0700

    Made the render loop run in a go block.
    Ended up changing several other things along the way. Introduced a State record. Rearchitected the basic flow of the rendering process so that it can accept external commands that have a method for the new update-state multimethod.
    
    Basically nothing has been tested except the also new handling of ^C, which should now shut down the rendering pipeline gracefully.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [fipp "0.6.2"]
                  [gloss "0.2.5"]]
   :main ^:skip-aot sparkle.core
--- src/sparkle/core.clj
+++ src/sparkle/core.clj
@@ -1,5 +1,9 @@
 (ns sparkle.core
   (:require [clojure.edn :as edn]
+            [clojure.core.async :as a
+             :refer [>! <! >!! <!! go go-loop chan buffer close! thread
+                     alt! alts! alts!! timeout]]
+            [clojure.tools.logging :as log]
             [fipp.edn :refer [pprint]]
             [sparkle.mode :as mode]
             [sparkle.color :as color]
@@ -10,17 +14,18 @@
 
 (def selected-model "test-strip")
 
-(println "Alive and kicking!")
+(defrecord State [env model mode-frame])
 
 (defn select-model [name model]
   (first (filter #(= name (% :model/name)) model)))
 
-; eval-mode returns a tree-structured map, that looks like this:
+; eval-state returns a tree-structured map, that looks like this:
 ; {:model-node {...}
 ;  :mode-frame {:mode mode :params params}
 ;  :children [{:child-name {child}} | [{child}]]}
 
-(defn eval-mode [env model {:keys [mode params] :as mode-frame}]
+(defn eval-state 
+  [{env :env model :model {:keys [mode params] :as mode-frame} :mode-frame}]
   (if (not (contains? model :model/children))
     {:model-node model
      :mode-frame mode-frame
@@ -32,51 +37,93 @@
        :mode-frame mode-frame
        :children (cond
                    (sequential? child-models)
-                     (map #(eval-mode env %1 %2) child-models child-mode-frames)
+                     (map #(eval-state (->State env %1 %2)) child-models child-mode-frames)
                    (map? child-models)
                      (into {}
                        (map (fn [child-name]
-                              {child-name (eval-mode
-                                            env
-                                            (child-name child-models)
-                                            (child-name child-mode-frames))})
+                              {child-name (eval-state
+                                            (->State
+                                              env
+                                              (child-name child-models)
+                                              (child-name child-mode-frames)))})
                             (keys child-models))))})))
 
 (defn setup []
   (let [model (select-model selected-model (edn/read-string (slurp model-file-path)))]
     {:model model}))
 
-(defn next-state [{:keys [model]}]
-  ; specify initial mode and params
-  ; provide env-map
-  (let [env {:time (System/currentTimeMillis)}]
-    {:model model
-     :mode mode/strip-blink
-     :params {:on-color {:r 156 :g 42 :b 0}
-              :off-color {:r 156 :g 42 :b 0}
-              :period 25}
-     :env env}))
+; This is where any automatic updates to env should happen.
+(defn next-state 
+  [{:keys [env model mode-frame]}]
+  (let [env (assoc-in env [:time] (System/currentTimeMillis))]
+    (map->State 
+      {:env env
+       :model model
+       :mode-frame 
+         {:mode mode/strip-blink
+          :params {:on-color {:r 156 :g 42 :b 0}
+                   :off-color {:r 156 :g 42 :b 0}
+                   :period 25}}})))
 
-(defn render [{:keys [model mode params env]}]
-  (let [mode-tree (eval-mode env model {:mode mode :params params})]
-    (fc/push-pixels (mode/pixel-map mode-tree))))
+(defn log-framerate [period]
+  (fn [step]
+    (let [cycle-start-time (atom (System/currentTimeMillis))
+          frame-count (atom 0)]
+      (fn
+        ([] (step))
+        ([accum] (step accum))
+        ([accum frame] 
+          (let [current-time (System/currentTimeMillis)
+                elapsed-cycle-time (- current-time @cycle-start-time)]
+            (if (> elapsed-cycle-time period)
+              (let [framerate (quot 1000 (/ elapsed-cycle-time @frame-count))]
+                (log/info "Framerate:" framerate)
+                (reset! cycle-start-time (System/currentTimeMillis))
+                (reset! frame-count 0))
+              (swap! frame-count inc))
+            (step accum frame)))))))
 
-(defn run []
-  (loop [last-state (setup)
-         cycle-start-time (System/currentTimeMillis)
-         frame-number 0]
-    (let [current-state (next-state last-state)
-          current-time (System/currentTimeMillis)
-          elapsed-cycle-time (- current-time cycle-start-time)]
-      (render (next-state current-state))
-      (if (> (- current-time cycle-start-time) 1000)
-        ; Report frame rate, restart cycle
-        (let [frame-rate (quot 1000 (/ elapsed-cycle-time frame-number))]
-          (println "Frame rate:" frame-rate)
-          (recur current-state (System/currentTimeMillis) 0))
-        ; Otherwise, continue
-        (recur current-state cycle-start-time (inc frame-number))))))
+
+(defmulti update-state 
+  (fn [command state] (:type command)))
+
+(defmethod update-state :stop [command state]
+  state)
+
+(defmethod update-state :render [command state]
+  (next-state state))
+
+(defn render-loop [command-chan render-chan]
+  (go-loop [last-state (setup)]
+    (let [command (alt!
+                    command-chan ([command] command)
+                    :default {:type :render})
+          state (update-state command last-state)]
+      (case (:type command)
+        :stop nil
+        :render (do
+                  (>! render-chan state)
+                  (recur state))
+        (recur state)))))
+
+(def render-pipeline
+  (comp
+    (map eval-state)
+    (map mode/pixel-map)
+    (log-framerate 1000)
+    ))
 
 (defn -main
   [& args]
-  (run))
+  (let [command-chan (chan (buffer 10))
+        render-chan (chan 1 render-pipeline)
+        render-loop-chan (render-loop command-chan render-chan)]
+    (go-loop []
+      (fc/push-pixels (<! render-chan)))
+    (.addShutdownHook (Runtime/getRuntime)
+      (Thread. (fn [] 
+                  (log/info "Recieved shutdown signal. Halting...")
+                  (>!! render-chan {:type :stop}))))
+    (<!! render-loop-chan)))
+
+(log/info "Alive and kicking!")
