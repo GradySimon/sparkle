521e63a17771bb1ab8ae301f75ba2ecf23152b0a
Grady Simon <grady@gradysimon.com>
2015-08-23 17:32:55 -0700

    Dropped the dependency on Gloss. Rewrote the push-pixel function. Seems to work.

--- src/sparkle/fadecandy_opc.clj
+++ src/sparkle/fadecandy_opc.clj
@@ -36,25 +36,42 @@
     :length :int16-be
     :pixels (gloss/repeated color :prefix :none)))
 
+(defn color-clip [value]
+  (min 255 (max 0 value)))
+
+(defn pixel->seq [{:keys [r g b]}]
+  (map color-clip [r g b]))
+
 (defn push-pixels [pixel-map]
   "Takes a map of channel numbers to vectors of pixels, packages
    them into a set-color packet, and pushes them to the FadeCandy
    device."
-  (doseq [[channel-num pixels] pixel-map]
-    (let [pixel-byte-count (* 3 (count pixels))]
-      (gloss-io/encode-to-stream set-color-packet @fc-conn
-        (seq
-          [{:channel-num channel-num
-            :command 0
-            :length pixel-byte-count
-            :pixels pixels}]))
-      (.flush @fc-conn)
-      (println (seq (.array (gloss-io/contiguous
-                        (gloss-io/encode set-color-packet
-                          {:channel-num channel-num
-                           :command 0
-                           :length pixel-byte-count
-                           :pixels pixels}))))))))
+   (doseq [[channel-num pixels] pixel-map]
+     (let [command-num 0 ; set color command per OPC
+           pixel-byte-length (* 3 (count pixels))
+           pixel-byte-length-high (quot pixel-byte-length 256)
+           pixel-byte-length-low (mod pixel-byte-length 256)
+           header [channel-num command-num pixel-byte-length-high pixel-byte-length-low]
+           pixel-bytes (mapcat pixel->seq pixels)
+           set-color-packet (byte-array (concat header pixel-bytes))]
+        (.write @fc-conn set-color-packet)
+        (.flush @fc-conn))))
+
+  ; (doseq [[channel-num pixels] pixel-map]
+  ;   (let [pixel-byte-count (* 3 (count pixels))]
+  ;     (gloss-io/encode-to-stream set-color-packet @fc-conn
+  ;       (seq
+  ;         [{:channel-num channel-num
+  ;           :command 0
+  ;           :length pixel-byte-count
+  ;           :pixels pixels}]))
+  ;     (.flush @fc-conn)
+  ;     (println (seq (.array (gloss-io/contiguous
+  ;                       (gloss-io/encode set-color-packet
+  ;                         {:channel-num channel-num
+  ;                          :command 0
+  ;                          :length pixel-byte-count
+  ;                          :pixels pixels}))))))))
 
 (defn start-pushing-pixels [pixel-chan]
   (go-loop []
