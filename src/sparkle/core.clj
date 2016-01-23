dd36963f365490fe6c9b865b57e87efe88a87964
Grady Simon <grady@gradysimon.com>
2016-01-23 15:57:20 -0800

    Create render and display loops wrapped in Components
    You can now start the whole system, push updates to its `layers`, and stop the system. The display loop prints a frame of LEDs every time it get a render frame that is different from the last one it printed.

--- src/sparkle/core.clj
+++ src/sparkle/core.clj
@@ -1,5 +1,6 @@
 (ns sparkle.core
-  (:require [clojure.algo.generic.functor :refer [fmap]]
+  (:require [clojure.core.async :refer [thread chan <!! >!! alt!! close!]]
+   [clojure.algo.generic.functor :refer [fmap]]
             [com.stuartsierra.component :as component])
   (:gen-class))
 
@@ -25,7 +26,6 @@
     (let [step (/ (- end-factor start-factor)
                   (- (count leds) 1))
           factors (iterate #(+ % step) start-factor)]
-      (println (take 5 factors))
       (map (fn [led factor]
              (fmap #(* factor %) led))
            leds factors))))
@@ -33,10 +33,38 @@
 (defn apply-layers [layers leds]
   ((apply comp layers) leds))
 
-(apply-layers [(brightness-gradient 0.8 0)
-               (scale-brightness 1)
-               (static-color white)]
-              leds)
+
+(defn display [{:keys [frame-chan] :as displayer} frame]
+  (>!! frame-chan frame))
+
+(defn start-displaying [{:keys [frame-chan] :as displayer}]
+  (thread
+    (loop [prev-frame nil]
+      (let [frame (<!! frame-chan)]
+        (if frame
+          (do (when (not= frame prev-frame)
+                (println frame))
+              (recur frame))
+          (println "Shutting down displayer"))))))
+
+(defrecord Displayer [frame-chan]
+  component/Lifecycle
+  
+  (start [displayer]
+    (let [frame-chan (chan)
+          displayer (assoc displayer :frame-chan frame-chan)]
+      (start-displaying displayer)
+      displayer))
+
+  (stop [displayer]
+    (close! frame-chan)
+    displayer))
+
+(defn render [layers displayer]
+  (display displayer (apply-layers layers leds)))
+
+(defn send-command [{:keys [command-chan] :as renderer} command]
+  (>!! command-chan command))
 
 (defmulti execute (fn [command layers status] (:type command)))
 
@@ -52,27 +80,46 @@
 (defmethod execute :stop [_ _ _]
   nil)
 
-(defn start-rendering [command-chan frame-chan]
-  (go-loop [layers []
-            status :running]
-    (let [[layers status :as loop-result]
-          (if (= status :running)
-            (alt! command-chan ([command] (execute command layers status)) 
-                  :default (do
-                             (render layers leds)
-                             [layers :running]))
-            (execute (<! command-chan) layers status))]
-      (when loop-result
-        (recur loop-result)))))
+(defn start-rendering [{:keys [command-chan displayer] :as renderer}]
+  (thread
+    (loop [layers []
+           status :running]
+      (let [[layers status :as loop-result]
+            (if (= status :running)
+              (alt!! command-chan ([command] (execute command layers status)) 
+                    :default (do
+                               (render layers displayer)
+                               [layers :running]))
+              (execute (<!! command-chan) layers status))]
+        (if loop-result
+          (recur layers status)
+          (println "Shutting down render loop"))))))
 
-; make the if :running render logic the default of alt?
-
-(defrecord Renderer [command-chan frame-chan]
+(defrecord Renderer [command-chan displayer]
   component/Lifecycle
 
   (start [renderer]
-    (start-rendering command-chan frame-chan)
-    (renderer))
+    (let [command-chan (chan)
+          renderer (assoc renderer :command-chan command-chan)]
+      (start-rendering renderer)
+      renderer))
 
   (stop [renderer]
-    (>! command-chan {:type :stop})))
+    (>!! command-chan {:type :stop})
+    renderer))
+
+
+(defn sparkle-system []
+  (component/system-map
+   :renderer (component/using (map->Renderer {})
+                              [:displayer])
+   :displayer (map->Displayer {})))
+
+;;; Below: defs useful for interacting with the running renderer
+
+(def started-system (component/start (sparkle-system)))
+
+(def renderer (:renderer started-system))
+
+(def displayer (:displayer started-system))
+
