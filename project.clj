b65a2e6e682f79a08b68d839592c99f4dce1e22a
Grady Simon <grady@gradysimon.com>
2016-06-12 16:11:45 -0700

    Bump Clojure to 1.9.0-alpha 5 for spec

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/algo.generic "0.1.2"]
                  [com.stuartsierra/component "0.3.1"]
